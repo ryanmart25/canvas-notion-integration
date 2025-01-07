02c977920386d8bc3371756aa408b022090bba1e
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2025-01-06 20:42:59 -0800

    completed Regex for Description Sanitization

--- src/main/java/Spot.java
+++ src/main/java/Spot.java
@@ -1,6 +1,6 @@
 package main.java;
 
 public class Spot {
-    public static final String ASSIGNMENTDESCRIPTIONREGEX = "</*p>|<a href=|</a>|&nbsp;|\"|\n";
+    public static final String ASSIGNMENTDESCRIPTIONREGEX = "\"|\n|<[a-zA-Z]*[\s]*[^>]*>|&nbsp;";
     public static final String ASSIGNMENTDESCRIPTIONREGEXREPLACEMENT = "";
 }
